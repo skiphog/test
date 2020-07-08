d8874abca79611750f37608240bfeeba0eb4c27e
Skip <skiphog@gmail.com>
2020-07-08 13:45:31 +0300

    Калькулятор

---
+++ 3/functions.php
@@ -0,0 +1,6 @@
+<?php
+
+function calc($a, $b, $operator)
+{
+    return [];
+}
---
+++ 3/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . '/functions.php';
+?>
+<form action="">
+    <input type="number">
+    <button>Посчитать</button>
+</form>
